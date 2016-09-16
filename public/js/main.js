994aead0948ce4b795d4618d98f86a132c212325
Rob Spectre <rob@brooklynhacker.com>
2016-09-16 14:13:09 +0200

    1) Added getDeviceId() to starting state.

--- public/js/main.js
+++ public/js/main.js
@@ -512,3 +512,11 @@ function createBird(item) {
   div.style.left = `${Math.floor(Math.random() * 600)}px`;
   return div;
 }
+
+function getDeviceId() {
+  return 'browser-' +
+    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+}
